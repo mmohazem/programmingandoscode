355b82fad463c4d6b5ce307873089d8924604f18
mmohazem <mh2200696@tkh.edu.eg>
2026-04-19 21:27:56 +0200

    final

--- security.cpp
+++ security.cpp
@@ -1,15 +1,13 @@
 #include "security.h"
 
-#define KEY 0xAA
-
 void encrypt(char* data, int length) {
     for (int i = 0; i < length; i++) {
-        data[i] ^= KEY;
+        data[i] = data[i] ^ KEY;
     }
 }
 
 void decrypt(char* data, int length) {
     for (int i = 0; i < length; i++) {
-        data[i] ^= KEY;
+        data[i] = data[i] ^ KEY;
     }
 }
